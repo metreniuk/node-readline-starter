c448c145609238927a71a058b8937405aaaea0f3
ametreniuc <metreniuk@gmail.com>
2019-02-28 21:16:56 +0200

    getQuality

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ rl.on("line", line => {
   main(lines)
 })
 
-// node index.js < a.in > a.out
-
-let graph = {}
+let graph = []
 
 function main(input) {
   let photos = input.slice(1).map((x, id) => {
@@ -27,5 +25,34 @@ function main(input) {
       tags,
     }
   })
-  console.log(photos)
+}
+
+// function createPath(graph) {
+//   let stack = []
+//   while(stack.length > 0) {
+
+//   }
+// }
+
+function getQuality(obj1, obj2) {
+  let res = []
+  let d1 = difference(obj1.tags, obj2.tags)
+  let d2 = difference(obj2.tags, obj1.tags)
+  let i = intersection(obj1.tags, obj2.tags)
+  res.push(d1.length)
+  res.push(d2.length)
+  res.push(i.length)
+  return Math.min(...res)
+}
+
+function intersection(aa, bb) {
+  let a = new Set(aa)
+  let b = new Set(bb)
+  return [...new Set([...a].filter(x => b.has(x))).values()]
+}
+
+function difference(aa, bb) {
+  let a = new Set(aa)
+  let b = new Set(bb)
+  return [...new Set([...a].filter(x => !b.has(x))).values()]
 }
